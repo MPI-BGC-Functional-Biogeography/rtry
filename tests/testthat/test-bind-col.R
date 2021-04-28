bdc5222a053c7fe091dfad0b56387b47e6773104
oleelamhy <oleelamhy@users.noreply.github.com>
2021-04-28 16:17:04 +0200

    Create test-bind-col.R
    Created basic tests for rtry_bind_col

---
+++ tests/testthat/test-bind-col.R
@@ -0,0 +1,32 @@
+context("Combine data by columns (rtry_bind_col)")
+
+
+test_that("basic test", {
+  df1 <- data.frame(
+    id = 1:10,
+    value = rnorm(10)
+  )
+
+  df2 <- data.frame(
+    char = letters[1:10]
+  )
+
+  df <- rtry_bind_col(df1, df2)
+
+  expect_equal(class(df), "data.frame")
+  expect_equal(ncol(df), 3)
+  expect_equal(nrow(df), 10)
+  expect_equal(ls(df), c("char", "id", "value"))
+})
+
+
+test_that("rtry_bind_col handles empty argument", {
+  message = "Please specify at least two data frames to be combined by columns."
+
+  expect_message(rtry_bind_col(), message)
+})
+
+
+test_that("rtry_bind_col handles NULL values", {
+  expect_equal(rtry_bind_col(NULL), NULL)
+})
