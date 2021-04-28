bc62a88b75de084dfa110475e9bb69d5a57ce6eb
oleelamhy <oleelamhy@users.noreply.github.com>
2021-04-28 18:20:52 +0200

    Create test-rm-col.R
    Created basic tests for rtry_rm_col

---
+++ tests/testthat/test-rm-col.R
@@ -0,0 +1,34 @@
+context("Remove data columns (rtry_rm_col)")
+
+
+test_that("basic test", {
+  df <- data.frame(
+    id = 1:10,
+    value = rnorm(10),
+    char1 = letters[1:10],
+    char2 = letters[11:20]
+  )
+
+  df_rm1 <- rtry_rm_col(df, char1)
+
+  expect_equal(class(df_rm1), "data.frame")
+  expect_equal(ncol(df_rm1), 3)
+  expect_equal(nrow(df_rm1), 10)
+  expect_equal(ls(df_rm1), c("char2", "id", "value"))
+
+  df_rm2 <- rtry_rm_col(df, char1, char2)
+
+  expect_equal(class(df_rm2), "data.frame")
+  expect_equal(ncol(df_rm2), 2)
+  expect_equal(nrow(df_rm2), 10)
+  expect_equal(ls(df_rm2), c("id", "value"))
+})
+
+
+test_that("rtry_rm_col handles empty or missing argument", {
+  message = "Please specify the input data and/or column names you would like to remove."
+
+  expect_message(rtry_rm_col(), message)
+  expect_message(rtry_rm_col(input = input), message)
+  expect_message(rtry_rm_col(... = col_names), message)
+})
