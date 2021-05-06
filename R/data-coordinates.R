578f8e4fe4c6d76cbb4d236e21b7d2b7e6fad7fb
oleelamhy <oleelamhy@users.noreply.github.com>
2021-05-06 21:06:08 +0200

    Add data descriptions
    Added descriptions for sample datasets `coordinates` and `locations`

---
+++ R/data-coordinates.R
@@ -0,0 +1,11 @@
+#' Sample coordinates data
+#'
+#' A dataset containing 20 sets of latitudes and longitudes in WGS84 projection.
+#'
+#' @format A data frame with 20 rows and 2 variables:
+#' \describe{
+#'   \item{Latitude}{latitude, in wgs84 projection}
+#'   \item{Longitude}{longitude, in wgs84 projection}
+#'   ...
+#' }
+"coordinates"
---
+++ R/data-locations.R
@@ -0,0 +1,12 @@
+#' Sample locations data
+#'
+#' A dataset containing 20 location information.
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \describe{
+#'   \item{Country code}{country code}
+#'   \item{Country}{full name of a country}
+#'   \item{Location}{specific location, e.g. town or city name}
+#'   ...
+#' }
+"locations"
