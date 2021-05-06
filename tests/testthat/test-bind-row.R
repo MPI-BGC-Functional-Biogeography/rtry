0035bd841d3112a059391a4e5ceb6430e630f163
oleelamhy <oleelamhy@users.noreply.github.com>
2021-05-06 13:36:15 +0200

    Update test-bind-row.R
    Added basic test for rtry_bind_row

--- tests/testthat/test-bind-row.R
+++ tests/testthat/test-bind-row.R
@@ -1,6 +1,26 @@
 context("Combine data by rows (rtry_bind_row)")
 
 
+test_that("basic test", {
+  df1 <- data.frame(
+    id = 1:10,
+    value = rnorm(10)
+  )
+
+  df2 <- data.frame(
+    id = 11:20,
+    value = rnorm(10)
+  )
+
+  df <- rtry_bind_row(df1, df2)
+
+  expect_equal(class(df), "data.frame")
+  expect_equal(ncol(df), 2)
+  expect_equal(nrow(df), 20)
+  expect_equal(ls(df), c("id", "value"))
+})
+
+
 test_that("rtry_bind_row handles empty argument", {
   message = "Please specify at least two data frames to be combined by rows."
 
