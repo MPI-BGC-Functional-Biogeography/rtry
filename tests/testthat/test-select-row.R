5a487e59fb8c74657f8f42ddbb322f6f3f7f9880
oleelamhy <oleelamhy@users.noreply.github.com>
2021-06-01 20:04:09 +0200

    Update test-select-row.R
    Added tests for rtry_select_row

--- tests/testthat/test-select-row.R
+++ tests/testthat/test-select-row.R
@@ -1,3 +1,35 @@
-test_that("", {
+context("Select data by rows (rtry_select_row)")
 
+
+test_that("basic test", {
+  df_sel_row1 <- rtry_select_row(TRYdata_14833, DataID %in% c(59, 60))
+
+  expect_equal(class(df_sel_row1), c("data.table", "data.frame"))
+  expect_equal(dim(df_sel_row1), c(323, 28))
+
+
+  df_sel_row2 <- df_sel_row <- rtry_select_row(TRYdata_14833, DataID %in% c(59))
+
+  expect_equal(class(df_sel_row2), c("data.table", "data.frame"))
+  expect_equal(dim(df_sel_row2), c(162, 28))
+
+
+  df_sel_row3 <- rtry_select_row(TRYdata_14833, DataID %in% c(59), getAuxiliary = TRUE)
+
+  expect_equal(class(df_sel_row3), c("data.table", "data.frame"))
+  expect_equal(dim(df_sel_row3), c(5708, 28))
+
+
+  df_sel_row4 <- rtry_select_row(TRYdata_14833, DataID %in% c(59), getAuxiliary = TRUE, rmDuplicates = TRUE)
+
+  expect_equal(class(df_sel_row4), c("data.table", "data.frame"))
+  expect_equal(dim(df_sel_row4), c(5669, 28))
+})
+
+
+test_that("rtry_select_row handles empty or missing argument", {
+  message = "Please specify the input data and/or criteria for row selection."
+
+  expect_message(rtry_select_row(), message)
+  expect_message(rtry_select_row(input = input), message)
 })
