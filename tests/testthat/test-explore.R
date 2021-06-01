4ac029a50c05081e979ab38dc69f6f49269f92a2
oleelamhy <oleelamhy@users.noreply.github.com>
2021-06-01 13:53:26 +0200

    Update test-explore.R
    Added tests for rtry_explore

--- tests/testthat/test-explore.R
+++ tests/testthat/test-explore.R
@@ -1,3 +1,18 @@
-test_that("", {
+context("Explore data (rtry_explore)")
 
+
+test_that("basic test", {
+  TRYdata_explore <- rtry_explore(TRYdata, DataID, DataName)
+
+  expect_equal(class(TRYdata_explore), c("tbl_df", "tbl", "data.frame"))
+  expect_equal(ncol(TRYdata_explore), 3)
+  expect_equal(colnames(TRYdata_explore), c("DataID", "DataName", "Count"))
+})
+
+
+test_that("rtry_explore handles one or more empty arguments", {
+  message = "Please specify the input data and/or attribute names you would like to group together."
+
+  expect_message(rtry_explore(), message)
+  expect_message(rtry_explore(TRYdata), message())
 })
