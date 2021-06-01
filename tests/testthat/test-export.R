fba1e21c9aeebbd03a109a00ed2f289c4f1ed60f
oleelamhy <oleelamhy@users.noreply.github.com>
2021-06-01 14:05:40 +0200

    Update test-export.R
    Added tests for rtry_export

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -1,3 +1,9 @@
-test_that("", {
+context("Export data (rtry_export)")
 
+
+test_that("rtry_export handles empty argument", {
+  message = "Please make sure you have specified the data to be saved or the output path."
+
+  expect_message(rtry_export(), message)
+  expect_message(rtry_export(TRYdata), message)
 })
