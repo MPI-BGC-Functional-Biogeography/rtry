315bc7ee1ef2cce609296363c87953409f13fdb6
oleelamhy <oleelamhy@users.noreply.github.com>
2021-06-01 20:08:29 +0200

    Update test-filter.R
    Added tests for rtry_filter

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,3 +1,17 @@
-test_that("", {
+context("Filter data (rtry_filter)")
 
+
+test_that("basic test", {
+  filter1 <- rtry_filter(TRYdata_14833, OrigValueStr %in% c("adult", "mature", "mature, healthy", "unknown"))
+
+  expect_equal(class(filter1), c("data.table", "data.frame"))
+  expect_equal(dim(filter1), c(5443, 28))
+})
+
+
+test_that("rtry_filter handles empty or missing argument", {
+  message = "Please specify the input data and/or criteria for filtering."
+
+  expect_message(rtry_filter(), message)
+  expect_message(rtry_filter(input = input), message)
 })
