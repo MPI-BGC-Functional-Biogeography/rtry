ed92aae5bd7384f9ff89a6fefc5e059254b70d38
oleelamhy <oleelamhy@users.noreply.github.com>
2021-06-10 13:55:14 +0200

    Update description for rtry_bind_col
    Updated function description for rtry_bind_col

--- R/bind_col.R
+++ R/bind_col.R
@@ -1,10 +1,15 @@
 #' Bind TRY data by columns
 #'
-#' This function takes a sequence of data imported by \code{rtry_import()} and combined them by columns.
+#' This function takes a sequence of data imported by \code{rtry_import()}
+#' and combined them by columns. The data should share the same number of rows.
 #'
 #' @param ... A sequence of data frame to be combined by columns
 #' @param showOverview Default \code{TRUE} displays the dimension and column names of the combined data
 #' @return A data table of the input data
+#' @examples
+#' \dontrun{
+#' rtry_bind_col(TRYdata1, TRYdata2)
+#' }
 #' @export
 rtry_bind_col <- function(..., showOverview = TRUE){
   if(missing(...)){
