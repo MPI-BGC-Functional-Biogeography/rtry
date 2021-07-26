5e1f0e3aac5ac943de590cd5414f1db41de81616
oleelamhy <oleelamhy@gmail.com>
2021-07-26 14:38:04 +0200

    Changed function documentation for rtry_explore

--- R/explore.R
+++ R/explore.R
@@ -2,7 +2,7 @@
 #'
 #' This function takes the data table imported by \code{rtry_import()} and converts it into
 #' a grouped data table based on the specified column names.
-#' Note the the output data is grouped by the first attribute if not specified using the parameter \code{sortBy}.
+#' Note that the output data are grouped by the first attribute if not specified using the parameter \code{sortBy}.
 #' To provide a first understanding of the data, an additional column is added to show the total count within each group.
 #'
 #' @param input Input data, imported by \code{rtry_import()} function or in data table format
@@ -12,7 +12,7 @@
 #' @return A data table of the unique values grouped by the desired attribute(s)
 #' @examples
 #' \dontrun{
-#' rtry_explore(TRYdata, AccSpeciesID, DataID, DataName, TraitID, TraitName)
+#' rtry_explore(TRYdata, TraitID, TraitName)
 #' rtry_explore(TRYdata, AccSpeciesID, DataID, DataName, TraitID, TraitName, sortBy = DataName)
 #' }
 #' @references \href{https://www.rdocumentation.org/packages/dplyr/versions/0.7.1/topics/group_by}{dplyr::group_by()}
