5823dc6cd0c9cfdb54a6520ffc8352d183884dec
oleelamhy <oleelamhy@gmail.com>
2021-07-27 13:15:10 +0200

    Added example in rtry_select_row documentation

--- R/select_row.R
+++ R/select_row.R
@@ -1,6 +1,6 @@
-#' Select TRY rows
+#' Select rows
 #'
-#' This function selects specified rows based on the specified criteria
+#' This function selects rows based on specified criteria
 #' and the corresponding \code{ObservationID} from the imported data for further processing.
 #'
 #' @param input Input data, imported by \code{rtry_import()} or in data table format
@@ -10,11 +10,17 @@
 #' @param showOverview Default \code{TRUE} displays the dimension of the data after row selection
 #' @return A data table of the selected rows of the input data
 #' @examples
-#' \dontrun{
-#' rtry_select_row(TRYdata, (TraitID > 0) | (DataID %in% c(59, 60)))
-#' rtry_select_row(TRYdata, TraitID %in% c(6), getAuxiliary = TRUE, rmDuplicates = TRUE)
-#' rtry_select_row(TRYdata, ErrorRisk < 4 | DataID %in% c(59, 60, 61, 6601, 327, 413, 1961, 210, 308))
-#' }
+#' # Select all traits records where DataID equals to 59 or 60
+#' # together with all the corresponding auxiliary data without any duplicates
+#' # within the provided sample data (TRYdata_15160)
+#' data_selected <- rtry_select_row(TRYdata_15160,
+#'                    (TraitID > 0) | (DataID %in% c(59, 60)),
+#'                    getAuxiliary = TRUE,
+#'                    rmDuplicates = TRUE)
+#'
+#' # Expected output:
+#' # 45 duplicates removed.
+#' # dim:   1737 28
 #' @seealso \code{\link{rtry_rm_dup}}
 #' @note This function by default filters data based on the unique identifier \code{ObservationID}
 #' listed in the TRY data, therefore, if the column \code{ObservationID} has been removed, this function
