5d8ead61cfe43a2432255b3118eb4b39efd55b97
oleelamhy <oleelamhy@gmail.com>
2021-07-28 16:53:55 +0200

    Renamed rtry_merge_x to rtry_join_x

--- R/join_outer.R
+++ R/join_outer.R
@@ -1,10 +1,10 @@
-#' Merge two data frames (left join)
+#' Outer join for two data frames
 #'
-#' This function merges two data frames based on a specified common column (by default: \code{ObservationID})
-#' and returns all records from the left data frame (\code{x}) together with the matched records
-#' from the right data frame (\code{y}), while discards all the records in the right data frame that
-#' does not exist in the left data frame. In order words, this functions performs a left join on
-#' the two provided data frames.
+#' This function merges two data frames based on the row names of the  specified common column
+#' (by default: \code{ObservationID}) and returns all rows from both tables, join records from
+#' the left (\code{x}) which have matching keys in the right data frame (\code{y}).
+#' In order words, this functions performs an outer join on the two provided data frames,
+#' i.e. the join table will contain all records from both the tables.
 #'
 #' @param x The left data frame, imported by \code{rtry_import()} or in data table format
 #' @param y The right data frame, imported by \code{rtry_import()} or in data table format
@@ -13,12 +13,13 @@
 #' @return A data table of the merged data
 #' @examples
 #' # Assume user has obtained a unique list of auxiliary data (e.g. Longitude and Latitude)
-#' # using rtry_select_aux() and would like to merge the georeferenced data into one data
-#' # table according to the identifier ObservationID and while the Longitude data exists
+#' # using rtry_select_aux() and would like to merge all the georeferenced data into one data
+#' # table according to the identifier ObservationID, does not matter if either Longitude or
+#' # Latitude data is not recorded
 #' lon <- rtry_select_aux(TRYdata_15160, Longitude)
 #' lat <- rtry_select_aux(TRYdata_15160, Latitude)
 #'
-#' georef <- rtry_merge_col(lon, lat)
+#' georef <- rtry_join_outer(lon, lat)
 #'
 #' # Expected output:
 #' # dim:   97 2
@@ -27,11 +28,11 @@
 #' # dim:   98 2
 #' # col:   ObservationID Latitude
 #' #
-#' # dim:   97 3
+#' # dim:   98 3
 #' # col:   ObservationID Longitude Latitude
-#' @seealso \code{\link{rtry_merge_row}}, \code{\link{rtry_bind_col}}
+#' @seealso \code{\link{rtry_join_left}}, \code{\link{rtry_bind_col}}, \code{\link{rtry_bind_row}}
 #' @export
-rtry_merge_col <- function(x = "", y = "", baseOn = ObservationID, showOverview = TRUE){
+rtry_join_outer <- function(x = "", y = "", baseOn = ObservationID, showOverview = TRUE){
   # Bind the variable OrigObsDataID locally to the function
   ObservationID <- NULL
 
@@ -40,13 +41,13 @@ rtry_merge_col <- function(x = "", y = "", baseOn = ObservationID, showOverview 
     message("Please specify the two data frames you would like to merge.")
   }
   else{
-    # Add quotations around the value in the baseOn argument
+    # Add quotations around the value in the baseOn argument when baseOn is not a character class
     col_name <- deparse(substitute(baseOn))
 
     # If the baseOn column exists in both data frame, continue the duplicates removal process
     if(col_name %in% colnames(x) && col_name %in% colnames(y)){
       # Perform left join using the merge function
-      mergedXY <- merge(x, y, by = col_name, all.x = TRUE)
+      mergedXY <- merge(x, y, by = col_name, all = TRUE)
 
       # If the argument showOverview is set to be TRUE, print the dimension and column names of the merged data
       if(showOverview == TRUE){
