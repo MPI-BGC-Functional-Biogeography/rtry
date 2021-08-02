f88b0cda995dbb3d28c969be4b6ea62b79ad8ebc
oleelamhy <oleelamhy@gmail.com>
2021-08-02 22:26:53 +0200

    Updated tests for rtry_bind_col

--- tests/testthat/test-bind-col.R
+++ tests/testthat/test-bind-col.R
@@ -2,21 +2,23 @@ context("Combine data by columns (rtry_bind_col)")
 
 
 test_that("basic test", {
-  df1 <- data.frame(
-    id = 1:10,
-    value = rnorm(10)
-  )
+  df1 <- rtry_select_col(TRYdata_15160,
+                           ObsDataID, ObservationID, AccSpeciesID, AccSpeciesName,
+                           ValueKindName, TraitID, TraitName, DataID, DataName,
+                           OrigObsDataID, ErrorRisk, Comment)
 
-  df2 <- data.frame(
-    char = letters[1:10]
-  )
+  df2 <- rtry_select_col(TRYdata_15160,
+                           OriglName, OrigValueStr, OrigUnitStr, StdValue, UnitName)
 
   df <- rtry_bind_col(df1, df2)
 
-  expect_equal(class(df), "data.frame")
-  expect_equal(ncol(df), 3)
-  expect_equal(nrow(df), 10)
-  expect_equal(colnames(df), c("id", "value", "char"))
+  expect_equal(class(df), c("data.table", "data.frame"))
+  expect_equal(ncol(df), 17)
+  expect_equal(nrow(df), 1782)
+  expect_equal(colnames(df), c("ObsDataID", "ObservationID", "AccSpeciesID", "AccSpeciesName",
+                               "ValueKindName", "TraitID", "TraitName", "DataID", "DataName",
+                               "OrigObsDataID", "ErrorRisk", "Comment",
+                               "OriglName", "OrigValueStr", "OrigUnitStr", "StdValue", "UnitName"))
 })
 
 
@@ -28,5 +30,5 @@ test_that("rtry_bind_col handles empty argument", {
 
 
 test_that("rtry_bind_col handles NULL values", {
-  expect_equal(rtry_bind_col(NULL), NULL)
+  expect_equal(dim(rtry_bind_col(NULL)), c(0, 0))
 })
