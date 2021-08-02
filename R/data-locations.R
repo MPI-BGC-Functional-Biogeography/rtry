f081678a8aa1460ecfa522e064a3281e267581ca
oleelamhy <oleelamhy@gmail.com>
2021-08-02 14:40:11 +0200

    Updated data descriptions

--- R/data-locations.R
+++ R/data-locations.R
@@ -4,9 +4,8 @@
 #'
 #' @format A data frame with 20 rows and 3 variables:
 #' \describe{
-#'   \item{Country code}{country code}
-#'   \item{Country}{full name of a country}
-#'   \item{Location}{specific location, e.g. town or city name}
-#'   ...
+#'   \item{Country code}{Country code.}
+#'   \item{Country}{Full name of a country.}
+#'   \item{Location}{Specific location, e.g. town or city name.}
 #' }
 "locations"
