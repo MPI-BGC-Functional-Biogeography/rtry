9688957e309cf22a9708f2edef34a889aafd490b
oleelamhy <oleelamhy@gmail.com>
2021-11-04 15:27:19 +0800

    Renamed rtry_filter to rtry_exclude

--- R/exclude.R
+++ R/exclude.R
@@ -1,26 +1,26 @@
-#' Filter data
+#' Exclude (remove) data
 #'
-#' This function takes the input data frame or data table and filters (excludes) all records (rows)
+#' This function takes the input data frame or data table and excludes all records (rows)
 #' with the same value in the attribute specified in the argument \code{baseOn} if the criteria
-#' specified in the arguments for filtering (\code{\dots}) are fulfilled for one of those records.
+#' specified in the arguments for excluding (\code{\dots}) are fulfilled for one of those records.
 #'
 #' @param input Input data frame or data table.
-#' @param \dots Criteria for filtering.
-#' @param baseOn The attribute on which filtering is based on. If it is set to \code{ObservationID},
-#'               the function filters all records with the respective \code{ObservationID} if the
-#'               specified criteria for filtering is fulfilled for one record. Alternatively, use
-#'               \code{ObsDataID} to filter only the record (row) for which the specified criterion
+#' @param \dots Criteria for excluding.
+#' @param baseOn The attribute on which excluding is based on. If it is set to \code{ObservationID},
+#'               the function excludes all records with the respective \code{ObservationID} if the
+#'               specified criteria for excluding is fulfilled for one record. Alternatively, use
+#'               \code{ObsDataID} to exclude only the record (row) for which the specified criterion
 #'               is fulfilled. Other reasonable parameter values are \code{TraitID}, \code{DataID}
 #'               or \code{AccSpeciesID}.
-#' @param showOverview Default \code{TRUE} displays the dimension of the data after filtering.
-#' @return An object of the same type as the input data after filtering.
+#' @param showOverview Default \code{TRUE} displays the dimension of the data after excluding.
+#' @return An object of the same type as the input data after excluding.
 #' @references This function makes use of the \code{\link[base]{subset}} function
 #'             within the \code{base} package.
 #' @examples
 #' # Example 1: Exclude observations on juvenile plants or unknown state:
 #' # Identify observations where the plant developmental status (DataID 413) is either
 #' # "juvenile" or "unknown", and exclude the whole observation
-#' data_filtered <- rtry_filter(TRYdata_15160,
+#' data_excluded <- rtry_exclude(TRYdata_15160,
 #'                    (DataID %in% 413) & (OrigValueStr %in% c("juvenile", "unknown")),
 #'                    baseOn = ObservationID)
 #'
@@ -30,20 +30,20 @@
 #' # Example 2: Exclude outliers:
 #' # Identify the outliers, i.e. trait records where the ErrorRisk is larger than 4
 #' # and exclude these records (not the whole observation)
-#' data_filtered <- rtry_filter(TRYdata_15160,
+#' data_excluded <- rtry_exclude(TRYdata_15160,
 #'                    ErrorRisk > 4,
 #'                    baseOn = ObsDataID)
 #'
 #' # Expected message:
 #' # dim:   1778 28
 #'
-#' # Learn more applications of the filtering function via the vignette (Workflow for
+#' # Learn more applications of the excluding function via the vignette (Workflow for
 #' # general data preprocessing using rtry): vignette("rtry-workflow-general").
 #' @export
-rtry_filter <- function(input, ..., baseOn, showOverview = TRUE){
+rtry_exclude <- function(input, ..., baseOn, showOverview = TRUE){
   # If either of the arguments input or ... is missing, show the message
   if(missing(input) || missing(...) || missing(baseOn)){
-    message("Please specify the input data and/or criteria and/or baseOn for filtering.")
+    message("Please specify the input data and/or criteria and/or baseOn for excluding.")
   }
   else{
     # Add quotations around the value in the baseOn argument
@@ -64,14 +64,14 @@ rtry_filter <- function(input, ..., baseOn, showOverview = TRUE){
     input <- subset(input, input$exclude == FALSE, select = -(exclude))
 
     # Copy the processed input into a new variable
-    filteredData <- input
+    excludedData <- input
 
-    # If the argument showOverview is set to be TRUE, print the dimension of the filtered data
+    # If the argument showOverview is set to be TRUE, print the dimension of the excluded data
     if(showOverview == TRUE){
-      message("dim:   ", paste0(dim(filteredData), sep = " "))
+      message("dim:   ", paste0(dim(excludedData), sep = " "))
     }
 
-    # Return the filtered data
-    return(filteredData)
+    # Return the excluded data
+    return(excludedData)
   }
 }
---
+++ tests/testthat/test-exclude.R
@@ -0,0 +1,20 @@
+context("Exclude (remove) data (rtry_exclude)")
+
+
+test_that("basic test", {
+  exclude1 <- rtry_exclude(TRYdata_15160,
+                         OrigValueStr %in% c("adult", "mature", "mature, healthy", "unknown"),
+                         baseOn = ObservationID)
+
+  expect_equal(class(exclude1), c("data.table", "data.frame"))
+  expect_equal(dim(exclude1), c(863, 28))
+})
+
+
+test_that("rtry_exclude handles empty or missing argument", {
+  message = "Please specify the input data and/or criteria and/or baseOn for excluding."
+
+  expect_message(rtry_exclude(), message)
+  expect_message(rtry_exclude(input = input), message)
+  expect_message(rtry_exclude(input = input, criteria), message)
+})
